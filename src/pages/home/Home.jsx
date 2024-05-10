677c748eec33d904a0873174693814ad6a1d8f5b
gtz.jesus <gtz.jesus@outlook.com>
2024-05-10 15:46:17 -0600

    File structure + displaying first component

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,35 @@
+// ------------------------------
+// File: Home.jsx
+// ------------------------------
+// Description: React complete Home page
+
+// ------------------------------
+// Imports
+// ------------------------------
+// This section has all necessary imports for this component.
+import styled from 'styled-components';
+import Navigation from '../../components/navigation/Navigation';
+
+/* Media query for larger devices */
+const ResponsiveContainer = styled.div`
+  color: red;
+  @media (min-width: 768px) {
+    max-width: 600px; /* Adjust as needed */
+    margin: 0 auto; /* Center the container horizontally */
+    padding: 0 20px;
+  }
+`;
+
+// ------------------------------
+// Component
+// ------------------------------
+// This section has our React Component which handles the data
+function Home() {
+  return (
+    <ResponsiveContainer>
+      <Navigation />
+    </ResponsiveContainer>
+  );
+}
+
+export default Home;
